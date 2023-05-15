510a4c38b955c95d1eed0c71b8baece6f89a6e33
Logan Cole <emtlogancole@gmail.com>
2023-05-15 15:26:41 -0500

    feat:get one market, happy path

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -2,4 +2,9 @@ class Api::V0::MarketsController < ApplicationController
   def index
     render json: MarketSerializer.format_markets(Market.all)
   end
+
+  def show
+    market = Market.find(params[:id])
+    render json:MarketSerializer.format_one_market(market)
+  end
 end
--- app/serializers/market_serializer.rb
+++ app/serializers/market_serializer.rb
@@ -15,4 +15,20 @@ class MarketSerializer
       }
     end
   end
+
+  def self.format_one_market(market)
+    Market.new
+      {
+        id: market.id,
+        name: market.name,
+        street: market.street,
+        city: market.city,
+        county: market.county,
+        state: market.state,
+        zip: market.zip,
+        lat: market.lat,
+        lon: market.lon,
+        vendor_count: market.vendor_count
+      }
+  end
 end

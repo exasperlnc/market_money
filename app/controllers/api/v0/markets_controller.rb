9a56ac4cad730eedc2039d69ea37d5208b40982c
Logan Cole <emtlogancole@gmail.com>
2023-05-15 19:53:36 -0500

    fist attempt at sad show market path

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -5,6 +5,10 @@ class Api::V0::MarketsController < ApplicationController
 
   def show
     market = Market.find(params[:id])
-    render json:MarketSerializer.new(market)
+    if market
+      render json:MarketSerializer.new(market)
+    else
+      ErrorMember.new("Couldn't find Market with 'id'=#{params[:id]}", "NOT FOUND", 404)
+    end
   end
 end
--- spec/requests/api/v0/markets_request_spec.rb
+++ spec/requests/api/v0/markets_request_spec.rb
@@ -1,18 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe 'Markets API' do
-  it 'gets all the markets' do
-    create_list(:market, 10)
+  describe 'gets all markets' do
+    it 'gets all the markets' do
+      create_list(:market, 10)
+    
+      get '/api/v0/markets'
+    
+      expect(response).to be_successful
+    
+      data = JSON.parse(response.body, symbolize_names: true)
+      markets = data[:data]
+      expect(markets.count).to eq(10)
+    
+      markets.each do |market|
+        expect(market).to have_key(:id)
+        expect(market[:attributes]).to have_key(:name)
+        expect(market[:attributes]).to have_key(:street)
+        expect(market[:attributes]).to have_key(:city)
+        expect(market[:attributes]).to have_key(:county)
+        expect(market[:attributes]).to have_key(:state)
+        expect(market[:attributes]).to have_key(:zip)
+        expect(market[:attributes]).to have_key(:lat)
+        expect(market[:attributes]).to have_key(:lon)
+      end
+    end
+  end
+  describe 'gets one market' do
+    it 'happy paht' do
+      create_list(:market, 2)
 
-    get '/api/v0/markets'
+      get "/api/v0/markets/#{Market.all.first.id}"
 
-    expect(response).to be_successful
+      expect(response).to be_successful
 
-    data = JSON.parse(response.body, symbolize_names: true)
-    markets = data[:data]
-    expect(markets.count).to eq(10)
+      data = JSON.parse(response.body, symbolize_names: true)
+      
+      market = data[:data]
 
-    markets.each do |market|
       expect(market).to have_key(:id)
       expect(market[:attributes]).to have_key(:name)
       expect(market[:attributes]).to have_key(:street)
@@ -22,6 +47,16 @@ RSpec.describe 'Markets API' do
       expect(market[:attributes]).to have_key(:zip)
       expect(market[:attributes]).to have_key(:lat)
       expect(market[:attributes]).to have_key(:lon)
+    end
+
+    it 'sad path' do
+      get "/api/v0/markets/1"
+
+      expect(response).to be_successful
+
+      data = JSON.parse(response.body, symbolize_names: true)
+      
+      market = data[:data]
     end
   end
 end

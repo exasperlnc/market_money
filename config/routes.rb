a4a8e091c8eb6cc1c9ef4fc9a40e459834baf934
Logan Cole <emtlogancole@gmail.com>
2023-05-16 14:36:23 -0500

    clean up routes file

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,4 @@ Rails.application.routes.draw do
       resources :vendors, only: [:show, :create]
     end
   end
-  # get 'api/v0/markets/:id/vendors', to: "vendors#index", as: 'vendors_index'
-  # get "/api/v0/markets/:market_id/vendors/:vendor_id", to: 'vendors#show'
 end

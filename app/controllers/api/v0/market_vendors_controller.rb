b07f95e6905d45d37a03102927b2ac8eeb27e801
Logan Cole <emtlogancole@gmail.com>
2023-05-17 13:54:08 -0500

    feat:delete market vendor

--- app/controllers/api/v0/market_vendors_controller.rb
+++ app/controllers/api/v0/market_vendors_controller.rb
@@ -1,4 +1,6 @@
 class Api::V0::MarketVendorsController < ApplicationController
+  before_action :find_market_vendor, only: [:destroy]
+
   def create
     market_vendor = MarketVendor.create(market_vendor_params)
     if market_vendor.valid?
@@ -8,7 +10,15 @@ class Api::V0::MarketVendorsController < ApplicationController
     end
   end
 
+  def destroy
+    render json: @market_vendor.destroy, status: 204
+  end
+
   private
+    def find_market_vendor
+      @market_vendor = MarketVendor.find(params[:id])
+    end
+
     def market_vendor_params
       params.require(:market_vendor).permit(:market_id, :vendor_id)
     end
--- spec/requests/api/v0/market_vendor_request_spec.rb
+++ spec/requests/api/v0/market_vendor_request_spec.rb
@@ -38,4 +38,42 @@ RSpec.describe "MarketVendors API" do
       expect(response.status).to eq(400)
     end
   end
+
+  describe 'deletes marketvendor' do
+    it 'happy path' do
+      vendor = create(:vendor)
+      market = create(:market)
+
+      market_vendor_params = ({
+        market_id: market.id,
+        vendor_id: vendor.id
+      })
+      
+      headers = {"CONTENT_TYPE" => "application/json"}
+      post "/api/v0/market_vendors", headers: headers, params: JSON.generate(market_vendor: market_vendor_params)
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+
+      market_vendor = MarketVendor.last
+
+      expect(market_vendor.market_id).to eq(market_vendor_params[:market_id])
+      expect(market_vendor.vendor_id).to eq(market_vendor_params[:vendor_id])
+
+      delete "/api/v0/market_vendors/#{MarketVendor.last.id}"
+
+      last_market_vendor = MarketVendor.last
+      expect(response).to be_successful
+
+      expect(last_market_vendor.market_id).to_not eq(market_vendor_params[:market_id])
+      expect(last_market_vendor.vendor_id).to_not eq(market_vendor_params[:vendor_id])
+    end
+
+    it 'sad path' do
+      delete "/api/v0/market_vendors/2918818238834"
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
+    end
+  end
 end

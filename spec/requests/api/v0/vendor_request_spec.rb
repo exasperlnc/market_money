47f039bc45c6e60224fe3827245dab62a0e2dc8c
Logan Cole <emtlogancole@gmail.com>
2023-05-17 12:07:42 -0500

    test: vendor delete happy path

--- spec/requests/api/v0/vendor_request_spec.rb
+++ spec/requests/api/v0/vendor_request_spec.rb
@@ -71,4 +71,33 @@ RSpec.describe "Vendors API" do
       expect(response).to_not be_successful
     end
   end
+
+  describe 'deletes vendor' do
+    it 'happy path' do
+      vendor_params = ({
+        name: 'Vendor 1',
+        description: 'Not your average vendor',
+        contact_name: 'John Smith',
+        contact_phone: '(123)456-7890',
+        credit_accepted: false
+      })
+    
+      headers = {"CONTENT_TYPE" => "application/json"}
+      post "/api/v0/vendors", headers: headers, params: JSON.generate(vendor: vendor_params)
+
+      expect(response).to be_successful
+      expect(response.status).to eq(201)
+
+      delete "/api/v0/vendors/#{Vendor.last.id}"
+
+      last_vendor = Vendor.last
+      expect(response).to be_successful
+
+      expect(last_vendor.name).to_not eq(vendor_params[:name])
+      expect(last_vendor.description).to_not eq(vendor_params[:description])
+      expect(last_vendor.contact_name).to_not eq(vendor_params[:contact_name])
+      expect(last_vendor.contact_phone).to_not eq(vendor_params[:contact_phone])
+      expect(last_vendor.credit_accepted).to_not eq(vendor_params[:credit_accepted])
+    end
+  end
 end

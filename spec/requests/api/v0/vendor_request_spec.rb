ea2098f3f37cb53f0c62812efc2ac69611a98502
Logan Cole <emtlogancole@gmail.com>
2023-05-17 12:11:27 -0500

    sort tests to correct request spec

--- spec/requests/api/v0/vendor_request_spec.rb
+++ spec/requests/api/v0/vendor_request_spec.rb
@@ -1,6 +1,68 @@
 require 'rails_helper'
 
 RSpec.describe "Vendors API" do
+  describe 'gets all vendors for a market' do
+    it 'happy path' do
+      market_1 = create(:market)
+
+      vendor_1 = create(:vendor)
+      vendor_2 = create(:vendor)
+
+      # MarketVendor.create(market_id: market_1.id, vendor_id: vendor_1.id)
+      # MarketVendor.create(market_id: market_1.id, vendor_id: vendor_2.id)
+
+      get "/api/v0/markets/#{market_1.id}/vendors"
+
+      data = JSON.parse(response.body, symbolize_names: true)
+      
+      vendors = data[:data]
+
+      
+      vendors.each do |vendor|
+        attributes = vendor[:attributes]
+        expect(vendor).to have_key(:id)
+        expect(attributes).to have_key(name)
+        expect(attributes.name).to be_a(String)
+        
+        expect(attributes).to have_key(description)
+        expect(attributes.description).to be_a(String)
+
+        expect(attributes).to have_key(contact_name)
+        expect(attributes.contact_name).to be_a(String)
+
+        expect(attributes).to have_key(contact_phone)
+        expect(attributes.description).to be_a(String)
+
+        expect(attributes).to have_key(credit_accepted)
+        expect(attributes.credit_accepted).to be_a(Boolean)
+
+      end
+    end
+
+    it 'sad path' do
+      get "/api/v0/markets/1231231231/vendors"
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
+
+    end
+  end
+
+  describe 'gets one vendor' do
+    it 'happy path' do
+      vendor_1 = create(:vendor)
+
+      get "/api/v0/vendors/#{vendor_1.id}"
+    end
+
+    it 'sad path' do
+      get "/api/v0/vendors/12398591"
+      
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
+    end
+  end
+
   describe 'creates vendor' do
     it 'happy path' do
       vendor_params = ({

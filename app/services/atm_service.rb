2b0be3793565026b732277b6c1bcc293e8435fb6
Logan Cole <emtlogancole@gmail.com>
2023-05-18 14:31:35 -0500

    atm service

---
+++ app/services/atm_service.rb
@@ -0,0 +1,16 @@
+class AtmService
+
+  def get_atms(lat, lon)
+    get_url("search/2/poiSearch/atm.json?&lat=#{lat}&lon=#{lon}")
+  end
+
+  def get_url(url)
+    response= conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn
+    Faraday.new(url: "https://api.tomtom.com") do |faraday|
+      faraday.params["key"] = ENV["atm_api"]
+  end
+end
